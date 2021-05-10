6c7a787c17837b5a70deda933b46b2d9547a5f61
Aman Kumar Shukla <77196231+theWiseAman@users.noreply.github.com>
2021-05-10 15:40:03 +0530

    Sudoku made playable, some CSS modified and hard coding for the paragraph removed

--- main.js
+++ main.js
@@ -67,12 +67,47 @@ var timer;
 var selectedNum;
 var selectedTile;
 var disableSelect;
+var startTime = new Date();
+var pause = false;
+var time;
 
 window.onload = function() {
+    let board = qs(".sudoku");
+    for (let i = 0; i < 81; i++) {
+        let p = document.createElement("p");
+        board.appendChild(p);
+    }
     var newGameBtn = id("newGame");
     newGameBtn.addEventListener("click", startGame());
     var reloadBtn= id("reload");
     reloadBtn.addEventListener("click", reload, false);
+    var submit = qs('[type="submit"]');
+    submit.addEventListener("click", checkSol);
+
+    setInterval(() => {
+        timer = id("timer");
+        if (!pause) 
+        timer.childNodes[1].innerText = elapsedTime(startTime);
+    }, 1000);
+
+    let numBtn = qs(".numBtn").children;
+    for (let i = 0; i < 9; i++) {
+        numBtn[i].addEventListener("click", function() {
+            if(!disableSelect) {
+                if (this.classList.contains("selected")) {
+                    this.classList.remove("selected");
+                    selectedNum = null;
+                } else {
+                    for (let i = 0; i < 9; i++)
+                        numBtn[i].classList.remove("selected");
+                    
+                    this.classList.add("selected");
+                    selectedNum = this;
+                    updateMove();
+                }
+            }
+        });
+    }
 }
 
 function id(id) {
@@ -89,16 +124,28 @@ function reload() {
     reload = location.reload();
 }
 
+function pauseTimer() {
+    pause = !pause;
+    let play = qs('[onclick="pauseTimer()"]');
+    if(pause) {
+        play.textContent = "Play";
+        time = timer.childNodes[1].innerText;
+    }
+    else {
+        play.textContent = "Pause";
+        timer.childNodes[1].innerText = time;
+    }
+} 
 function startGame() {
-    var diffLevel = id("diffLevel");
-    var diff = id("difficulty");
+    let diffLevel = id("diffLevel");
+    let diff = id("difficulty");
     diffLevel.textContent = diff.options[diff.selectedIndex].text;
 
     disableSelect = false;
-    
-    var timer = id("timer");
 
     generateBoard(diff.value);
+
+    startTime = new Date();
 }
 
 function generateBoard(diffLevel) {
@@ -122,8 +169,33 @@ function generateBoard(diffLevel) {
     for (let i = 0; i < 9; i++)
     for (let j = 0; j < 9; j++, k++) {
         if (pos.includes(k)) p[k].textContent = sudoku[i][j];
+        else {
+            p[k].classList.add("blankTile");
+            p[k].addEventListener("click", function() {
+                if(!disableSelect) {
+                    if(this.classList.contains("selected")) {
+                        this.classList.remove("selected");
+                        selectedTile = null;
+                    }
+                    else {
+                        for (let i = 0; i < 81; i++)
+                            p[i].classList.remove("selected");
+
+                        this.classList.add("selected");
+                        selectedTile = this;
+                        updateMove();
+                    }
+                }
+            });
+        } 
     }
     
+}
+function updateMove() {
+    if(selectedTile && selectedNum) {
+        selectedTile.textContent = selectedNum.textContent;
+        selectedNum = null;
+    }
 }
 
 function clearPrevious() {
@@ -137,4 +209,30 @@ function clearPrevious() {
     selectedNum = null;
 }
 
-
+function elapsedTime(startTime) {
+    let endTime = new Date();
+    let timeDiff = endTime.getTime() - startTime.getTime();
+    timeDiff /= 1000;
+    let ss = Math.floor(timeDiff % 60);
+    ss = ss < 10 ? "0" + ss : ss;
+    timeDiff = Math.floor(timeDiff / 60);
+    let mm = timeDiff % 60;
+    mm = mm < 10 ? "0" + mm : mm;
+    timeDiff = Math.floor(timeDiff / 60);
+    let hh = timeDiff;
+    hh = hh < 10 ? "0" + hh : hh;
+    if (hh === "00") {
+        return mm + ":" + ss;
+    } else {
+        return hh + ":" + mm + ":" + ss;
+    }
+}
+function checkSol () {
+    let p = qsa("p");
+    for (let i = 0, k = 0; i < 9; i++)
+    for(let j = 0; j < 9; j++, k++)
+    if(parseInt(p[k].textContent) !== sudoku[i][j]) {
+        return alert("Wrong Solution!! Continue solving the cryptic...");
+    }
+    return alert("You cracked the cryptic!!");
+}
